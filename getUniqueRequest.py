984f3bb56e828cd62b9bc2741b9646adefd8a73a
Subham Kundu <43017632+Cenrax@users.noreply.github.com>
2022-03-17 14:04:03 +0530

    Update getUniqueRequest.py

--- getUniqueRequest.py
+++ getUniqueRequest.py
@@ -1,20 +1,5 @@
 import csv
 
-log = open('example_log.txt')
-parsed = parse_log(log)
-file = open('temporary.csv', 'r+')
-csv_file = build_csv(
-    parsed,
-    file,
-    header=[
-        'ip', 'time_local', 'request_type',
-        'request_path', 'status', 'bytes_sent',
-        'http_referrer', 'http_user_agent'
-    ]
-)
-import csv
-import itertools
-
 def count_unique_request(csv_file):
     reader = csv.reader(csv_file)
     header = next(reader)
@@ -22,10 +7,12 @@ def count_unique_request(csv_file):
     
     uniques = {}
     for line in reader:
+        
         if not uniques.get(line[idx]):
             uniques[line[idx]] = 0
         uniques[line[idx]] += 1
     return uniques
+
 
 log = open('example_log.txt')
 parsed = parse_log(log)
@@ -40,7 +27,32 @@ csv_file = build_csv(
     ]
 )
 uniques = count_unique_request(csv_file)
-print(uniques)
+def count_unique_request(csv_file):
+    reader = csv.reader(csv_file)
+    header = next(reader)
+    idx = header.index('request_type')
+    
+    uniques = {}
+    for line in reader:
+        
+        if not uniques.get(line[idx]):
+            uniques[line[idx]] = 0
+        uniques[line[idx]] += 1
+    return ((k, v) for k,v in uniques.items())
+
+log = open('example_log.txt')
+parsed = parse_log(log)
+file = open('temporary.csv', 'r+')
+csv_file = build_csv(
+    parsed,
+    file,
+    header=[
+        'ip', 'time_local', 'request_type',
+        'request_path', 'status', 'bytes_sent',
+        'http_referrer', 'http_user_agent'
+    ]
+)
+uniques = count_unique_request(csv_file)
 summarized_file = open('summarized.csv', 'r+')
 summarized_csv = build_csv(uniques, summarized_file, header=['request_type', 'count'])
 print(summarized_file.readlines())
