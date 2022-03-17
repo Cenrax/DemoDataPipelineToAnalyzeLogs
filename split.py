4713efa8e1fddd867bafb065ee5b17d6582bd11c
Subham Kundu <43017632+Cenrax@users.noreply.github.com>
2022-03-17 12:35:10 +0530

    Create split.py

---
+++ split.py
@@ -0,0 +1,18 @@
+log = open('example_log.txt')
+def parse_log(log):
+    for line in log:
+        split_line = line.split()
+        remote_addr = split_line[0]
+        time_local = split_line[3] + " " + split_line[4]
+        request_type = split_line[5]
+        request_path = split_line[6]
+        status = split_line[8]
+        body_bytes_sent = split_line[9]
+        http_referrer = split_line[10]
+        http_user_agent = " ".join(split_line[11:])
+        yield (
+            remote_addr, time_local, request_type, request_path,
+            status, body_bytes_sent, http_referrer, http_user_agent
+        )
+
+first_line = next(parse_log(log))
